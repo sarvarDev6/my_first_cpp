d177a471c20a1321450866a073d94c7f9bb3795a
sarvarDev6 <sarvardev5@gmail.com>
2024-12-01 17:10:24 +0500

    first

---
+++ veriables.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main () {
+    // int x = 5;
+    // int y = 6;
+    // int sum = x + y;
+
+    // std::cout << x << '\n';
+    // std::cout << y << '\n';
+    // std::cout << sum << '\n';
+
+
+    //? Integer (whole numbers)
+    int age = 14;
+    int year = 2024;
+    int days = 7.5;
+
+    //? Double (number including  decimal)
+    double price = 10.99;
+
+    std::cout << price;
+
+    return 0;
+}
